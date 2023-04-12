1a3684e8e628e781c98937dfea65b05d784ea490
nastiaknik <anastazja.knihnitska@gmail.com>
2023-04-12 18:38:31 +0200

    Optimized selectors with createSelector for better performance and reduced re-renders
    Refactored selectors using the createSelector function for improved performance and reduced re-renders. The createSelector function creates memoized selectors, which cache their input arguments and only recalculate when the inputs change, resulting in faster and more efficient selector behavior.

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,11 +1,11 @@
+import { createSelector } from '@reduxjs/toolkit';
 import { statusFilters } from './contansts';
 
 export const selectTasks = state => state.tasks;
 
 export const selectStatusFilter = state => state.filters.status;
 
-export const selectTaskCount = state => {
-  const tasks = selectTasks(state);
+export const selectTaskCount = createSelector([selectTasks], tasks => {
   return tasks.reduce(
     (count, task) => {
       if (task.completed) {
@@ -17,17 +17,18 @@ export const selectTaskCount = state => {
     },
     { active: 0, completed: 0 }
   );
-};
+});
 
-export const selectVisibleTasks = state => {
-  const tasks = selectTasks(state);
-  const statusFilter = selectStatusFilter(state);
-  switch (statusFilter) {
-    case statusFilters.active:
-      return tasks.filter(task => !task.completed);
-    case statusFilters.completed:
-      return tasks.filter(task => task.completed);
-    default:
-      return tasks;
+export const selectVisibleTasks = createSelector(
+  [selectTasks, selectStatusFilter],
+  (tasks, statusFilter) => {
+    switch (statusFilter) {
+      case statusFilters.active:
+        return tasks.filter(task => !task.completed);
+      case statusFilters.completed:
+        return tasks.filter(task => task.completed);
+      default:
+        return tasks;
+    }
   }
-};
+);
